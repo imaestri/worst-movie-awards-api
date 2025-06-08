eec615a091e7e3dd4be398dbfbc5f1be0b2c62c1
Isaque Maestri <isaquemaestri01@gmail.com>
2025-06-08 17:29:47 -0300

    feat: Add Jest configuration and update test command in package.json

---
+++ jest.config.js
@@ -0,0 +1,5 @@
+export default {
+  testMatch: ["**/tests/**/*.[jt]s?(x)", "**/tests/**/*.mjs"], // adiciona suporte a .mjs
+  transform: {}, // desativa transformações automáticas
+  testEnvironment: "node",
+};
